e928bcef34f03ad132760f63cf2d476cd339766f
Daniel Morala <morala84@gmail.com>
2020-10-16 20:15:55 +0200

    DI - Práctica 01

--- DI - Practica_1/codejs.js
+++ DI - Practica_1/codejs.js
@@ -4,6 +4,7 @@ var unidadesItem;
 var errorNombre;
 var totalNombre;
 var precioSuma = 0;
+var expPrecio = /\d+(,\d+)?/;
 listaItem = [];
 
 function inicializar() {
@@ -13,7 +14,35 @@ function inicializar() {
     errorNombre = document.getElementById("errorNombre");
     errorPrecio = document.getElementById("errorPrecio");
     artTotal = document.formulario.artCarro;
-    precioTotal = document.formulario.precioCarro;  
+    precioTotal = document.formulario.precioCarro; 
+    tipoPago = document.formulario.tipoPago;
+    tarjeta.style.display = "none";
+    efectivo.style.display = "none";
+    checkBox = document.formulario.condiciones;
+    imprimir = document.formulario.imprimir;
+}
+
+function condicion(){ 
+    if (checkBox.checked == true) {
+        imprimir.disabled = false;
+    } else {
+        imprimir.disabled = true;
+    }
+}
+
+function seleccionPago() {
+    if (tipoPago.value == "seleccion"){
+        tarjeta.style.display = "none";
+        efectivo.style.display = "none";
+
+    }else if (tipoPago.value == "visa"){
+        tarjeta.style.display = "block";
+        efectivo.style.display = "none";
+
+    }else if (tipoPago.value == "efectivo"){
+        tarjeta.style.display = "none";
+        efectivo.style.display = "block";
+    }
 }
 
 function reset(){
@@ -24,6 +53,18 @@ function reset(){
     errorPrecio.innerHTML = "";
 }
 
+function resetTotal(){
+    nombreItem.value = "";
+    precioItem.value = "";
+    precioSuma = 0;
+    unidadesItem.value = 1;
+    errorNombre.innerHTML = "";
+    errorPrecio.innerHTML = "";
+    listaItem.length = 0;
+    artTotal.value ="";
+    precioTotal.value ="";
+}
+
 function verPrecios() {
     precioSuma +=(precioItem.value * unidadesItem.value);
     precioTotal.value = precioSuma;
@@ -31,11 +72,14 @@ function verPrecios() {
 
 function verArticulos() {
     listaItem.push(nombreItem.value);
+
     for (var i = 0; i < listaItem.length; i++) {
+
         if (i == 0) {
             totalNombre = listaItem[i];
+
         } else {
-            totalNombre += " ," + listaItem[i];
+            totalNombre += ", " + listaItem[i];
         }
     }
     artTotal.value = totalNombre;
@@ -55,31 +99,43 @@ function validacionPrecio() {
         errorPrecio.style.color = "red";
         return true;
     }
+    if (!expPrecio.test(precioItem.value)) {
+        errorPrecio.innerHTML = "Tipo de dato incorrecto.";
+        errorPrecio.style.color = "red";
+        return true;
+    }     
 }
 
 function eventos() {
     document.formulario.addCarro.addEventListener("click", addCarrito);
+    tipoPago.addEventListener("change", seleccionPago);
+    checkBox.addEventListener("change", condicion);
+    document.formulario.restablecer.addEventListener("click", resetTotal);
 }
 
 function addCarrito() {
-    
     validacionNombre();
     validacionPrecio();
-    if (validacionPrecio()) {
-        precioItem.focus();
-        errorNombre.innerHTML = "";
+    
+    if (validacionPrecio() && validacionNombre()) {
+        nombreItem.focus();
+
     }else if (validacionNombre()){
         nombreItem.focus();
         errorPrecio.innerHTML = "";
+
+    }else if (validacionPrecio()){
+        precioItem.focus();
+        errorNombre.innerHTML = "";
+
     }else{
         verArticulos();
         verPrecios();
-        reset();
-    }
-    
+        reset();  
+    } 
 }
 
 window.onload = function () {
     inicializar();
     eventos();
-};
+}

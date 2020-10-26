5d900ddef0e6d2b6235f406561fbe166fdeaa208
Daniel Morala <morala84@gmail.com>
2020-10-26 21:00:12 +0100

    PSP - Practica 2

---
+++ PSP - Practica_2/src/Cola.java
@@ -0,0 +1,25 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import Correos.Correo;
+
+
+public class Cola {
+
+    public static final int COLA_MAXIMO = 5;
+
+    private Queue <Correo> cola = new LinkedList<>();
+
+    private synchronized void addMensaje(Correo correo) {
+
+        while (cola.size() == COLA_MAXIMO){
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            wait();
+        }
+        cola.offer(correo);
+        notify();
+    } 
+}

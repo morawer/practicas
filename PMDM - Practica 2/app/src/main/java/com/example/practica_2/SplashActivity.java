e0181308d53fe4076047ceba6f58ade504e4e43f
Daniel Morala <morala84@gmail.com>
2021-01-21 20:20:05 +0100

    Modificaciones varias.

--- PMDM - Practica 2/app/src/main/java/com/example/practica_2/SplashActivity.java
+++ PMDM - Practica 2/app/src/main/java/com/example/practica_2/SplashActivity.java
@@ -2,13 +2,36 @@ package com.example.practica_2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.animation.Animation;
 
-public class SplashActivity extends AppCompatActivity {
+public class SplashActivity extends AppCompatActivity implements Animation.AnimationListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        getActionBar().hide();
+
+    }
+
+    @Override
+    public void onAnimationStart(Animation animation) {
+
+    }
+
+    @Override
+    public void onAnimationEnd(Animation animation) {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+        finish();
+
+
+    }
+
+    @Override
+    public void onAnimationRepeat(Animation animation) {
+
     }
 }

0ab4920fb1ab5a5beede7dc43f3b65c000548b58
Daniel Morala <morala84@gmail.com>
2021-01-21 20:18:26 +0100

    Login creado.

--- PMDM - Practica 2/app/src/main/java/com/example/practica_2/LoginActivity.java
+++ PMDM - Practica 2/app/src/main/java/com/example/practica_2/LoginActivity.java
@@ -3,6 +3,10 @@ package com.example.practica_2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +14,18 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        getSupportActionBar().hide();
+    }
+
+    public void crearUsuario(View view) {
+        Toast toast = Toast.makeText(this, "Función no disponible.", Toast.LENGTH_LONG );
+        toast.show();
+    }
+
+    public void entrarButon (View view) {
+        TextInputEditText usuario = (TextInputEditText) findViewById(R.id.user);
+        TextInputEditText pass = (TextInputEditText) findViewById(R.id.password);
+
+
     }
 }

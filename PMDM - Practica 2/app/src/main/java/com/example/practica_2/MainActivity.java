2a87f8f840f7786529f80cecc7344970dcc3826d
Daniel Morala <morala84@gmail.com>
2021-01-24 18:59:56 +0100

    Creada DB para tareas.

--- PMDM - Practica 2/app/src/main/java/com/example/practica_2/MainActivity.java
+++ PMDM - Practica 2/app/src/main/java/com/example/practica_2/MainActivity.java
@@ -9,15 +9,26 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.Toast;
 
+import com.example.practica_2.db.ControladorDB;
+
 public class MainActivity extends AppCompatActivity {
+
+    ControladorDB controladorDB;
+    private ArrayAdapter<String> miAdapter;
+    ListView listViewTareas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        controladorDB = new ControladorDB(this);
+        listViewTareas = (ListView) findViewById(R.id.listaTareas);
+        actualizarUI();
     }
 
     @Override
@@ -28,19 +39,30 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        EditText cajaTexto = new EditText(this);
+        final EditText cajaTexto = new EditText(this);
         AlertDialog dialog = new AlertDialog.Builder(this)
                 .setMessage("Escribe la tarea.")
                 .setView(cajaTexto)
                 .setPositiveButton("Añadir", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialogInterface, int i) {
-
+                        String tarea = cajaTexto.getText().toString();
+                        controladorDB.addTarea(tarea);
+                        actualizarUI();
                     }
                 })
                 .setNegativeButton("Cancelar", null)
                 .create();
         dialog.show();
         return super.onOptionsItemSelected(item);
+    }
+
+    private void actualizarUI() {
+        if (controladorDB.numeroRegistros() == 0) {
+            listViewTareas.setAdapter(null);
+        } else {
+            miAdapter = new ArrayAdapter<>(this, R.layout.item_tarea, R.id.textItem, controladorDB.obtenerTareas());
+            listViewTareas.setAdapter(miAdapter);
+        }
     }
 }
--- PMDM - Practica 2/app/src/main/java/com/example/practica_2/db/ControladorDB.java
+++ PMDM - Practica 2/app/src/main/java/com/example/practica_2/db/ControladorDB.java
@@ -1,20 +1,22 @@
 package com.example.practica_2.db;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 import androidx.annotation.Nullable;
 
 public class ControladorDB extends SQLiteOpenHelper {
-    public ControladorDB(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+    public ControladorDB(@Nullable Context context) {
         super(context, "com.example.practica_2.db", null, 1);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
 
-        db.execSQL("CREATE TABLE TAREAS (ID INTERGER PRIMARY KEY AUTOINCREMENT, NOMBRE TEXT NOT NULL);");
+        db.execSQL("CREATE TABLE TAREAS (ID INTEGER PRIMARY KEY AUTOINCREMENT, NOMBRE TEXT NOT NULL);");
     }
 
     @Override
@@ -22,7 +24,44 @@ public class ControladorDB extends SQLiteOpenHelper {
 
     }
 
-    public void addTarea (){
+    public void addTarea(String tarea) {
+
+        ContentValues registro = new ContentValues();
+        registro.put("NOMBRE", tarea);
+
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        db.insert("TAREAS", null, registro);
+        db.close();
+    }
+
+    public String[] obtenerTareas() {
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        Cursor cursor = db.rawQuery("SELECT * FROM TAREAS", null);
+
+        int regs = cursor.getCount();
+
+        if (regs == 0) {
+            db.close();
+            return null;
+        } else {
+            String[] tareas = new String[regs];
+            cursor.moveToFirst();
+            for (int i = 0; i < regs; i++) {
+                tareas[i] = cursor.getString(1);
+                cursor.moveToNext();
+            }
+            db.close();
+            return tareas;
+        }
+    }
+
+    public int numeroRegistros() {
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        Cursor cursor = db.rawQuery("SELECT * FROM TAREAS", null);
+        return cursor.getCount();
 
     }
 }
